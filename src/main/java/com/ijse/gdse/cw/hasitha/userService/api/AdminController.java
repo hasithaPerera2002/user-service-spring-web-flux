7fdb7547bb47796a6d3d19804f9376c7458eda9e
Hasitha Perera <hasithaperera2017@.com>
2023-10-09 16:09:05 +0530

    controller methods done

---
+++ src/main/java/com/ijse/gdse/cw/hasitha/userService/api/AdminController.java
@@ -0,0 +1,9 @@
+package com.ijse.gdse.cw.hasitha.userService.api;/********************************************************************
+ * --
+ * Author: hasit
+ * Date: 10/9/2023
+ * --
+ ********************************************************************/
+
+public class AdminController {
+}
